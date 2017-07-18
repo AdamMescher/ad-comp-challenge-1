7cbf39ffa3e51be4ea5718be75ab44e0a0df7c65
Adam Mescher <admescher@gmail.com>
2017-07-18 10:56:33 -0600

    final commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// ABOUT US BUTTON HOVER
+
+$('.about-button').mouseover(function(){
+  $('.about-button-icon').prop("src", "images/icons/icon-about-black.svg");
+})
+
+$('.about-button').mouseleave(function(){
+  $('.about-button-icon').prop("src", "images/icons/icon-about-white.svg");
+})
+
+// CONTACT US BUTTON HOVER
+
+$('.contact-button').mouseover(function(){
+  $('.contact-button-icon').prop("src", "images/icons/icon-contact-black.svg");
+})
+
+$('.contact-button').mouseleave(function(){
+  $('.contact-button-icon').prop("src", "images/icons/icon-contact-white.svg");
+})
+
+// HISTORY BOX HOVER
+
+$('.history-box').mouseover(function(){
+  $('.history-box-icon').prop("src", "images/icons/icon-bottle-cap-red.svg");
+})
+
+$('.history-box').mouseleave(function(){
+  $('.history-box-icon').prop("src", "images/icons/icon-bottle-cap-white.svg");
+})
+
+// SUSTAINABILITY BOX HOVER
+$('.sustainability-box').mouseover(function(){
+  $('.sustainability-box-icon').prop("src", "images/icons/icon-sustainability-black.svg");
+})
+
+$('.sustainability-box').mouseleave(function(){
+  $('.sustainability-box-icon').prop("src", "images/icons/icon-sustainability-white.svg");
+})
+
+// BRANDS BOX HOVER
+$('.brands-box').mouseover(function(){
+  $('.brands-box-icon').prop("src", "images/icons/icon-bottle-red.svg");
+})
+
+$('.brands-box').mouseleave(function(){
+  $('.brands-box-icon').prop("src", "images/icons/icon-bottle-white.svg");
+})
+
+// CAREERS BOX HOVER
+
+$('.careers-box').mouseover(function(){
+  $('.careers-box-icon').prop("src", "images/icons/icon-careers-black.svg");
+})
+
+$('.careers-box').mouseleave(function(){
+  $('.careers-box-icon').prop("src", "images/icons/icon-careers-white.svg");
+})
+
+// NUTRITION BOX HOVER
+
+$('.nutrition-box').mouseover(function(){
+  $('.nutrition-box-icon').prop("src", "images/icons/icon-nutrition-black.svg");
+})
+
+$('.nutrition-box').mouseleave(function(){
+  $('.nutrition-box-icon').prop("src", "images/icons/icon-nutrition-white.svg");
+})
+
+// PRESS BOX HOVER
+
+$('.press-box').mouseover(function(){
+  $('.press-box-icon').prop("src", "images/icons/icon-press-red.svg");
+})
+
+$('.press-box').mouseleave(function(){
+  $('.press-box-icon').prop("src", "images/icons/icon-press-white.svg");
+})
+
+// INVESTORS BOX HOVER
+
+$('.investors-box').mouseover(function(){
+  $('.investors-box-icon').prop("src", "images/icons/icon-investors-green.svg");
+})
+
+$('.investors-box').mouseleave(function(){
+  $('.investors-box-icon').prop("src", "images/icons/icon-investors-white.svg");
+})
